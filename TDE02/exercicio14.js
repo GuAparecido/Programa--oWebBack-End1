768cfeb9b0942b4ba011aa34ea026e77307aea59
GuAparecido <gustavorcdaparecido@gmail.com>
2024-04-30 22:56:32 -0300

    Aula05 - Iniciando o projeto

--- TDE02/exercicio14.js
+++ TDE02/exercicio14.js
@@ -1,7 +1,7 @@
 // Escreva uma função que receba dois números como parâmetros e retorne verdadeiro se a soma deles 
 // for divisível por 5 e falso caso contrário.
-function divisivelPor5(num1, num2){
+function somaDivisivelPor5(num1, num2){
     return ((num1 + num2) % 5 == 0);
 }
 
-console.log(divisivelPor5(5, 6));
+console.log(somaDivisivelPor5(5, 6));
