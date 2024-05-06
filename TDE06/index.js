06bfb235ae117b125d3aae632c98fc8baf873b86
Gustavo Aparecido <gustavorcdaparecido@gmail.com>
2024-05-06 20:00:56 -0300

    Aula 06

---
+++ TDE06/index.js
@@ -0,0 +1,20 @@
+/* Crie um projeto node.
+- Configure um script de start
+- Configure um script dev utilizando o nodemon
+- Crie um servidor express que rode na porta 5000
+- Crie uma rota GET /health que retorne um JSON: {"status": "running"} */
+const express = require("express");
+const server = express();
+
+//http://localhost:5000
+
+const port = 5000;
+server.listen(port, () => {
+    console.log(`Server rodando na porta: ${port}`);	
+})
+
+server.get("/health", (req, res) => {
+    res.json ({
+        status: "running"
+    })
+})
