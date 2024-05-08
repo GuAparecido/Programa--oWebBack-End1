a2a651a4218612e59c3d449aa37ba3edaa3190d8
Gustavo Aparecido <gustavorcdaparecido@gmail.com>
2024-05-08 20:51:31 -0300

    Add Prisma

--- project/routes/movies.js
+++ project/routes/movies.js
@@ -1,69 +1,89 @@
 const express = require("express")
+const {prisma} = require("../db/prisma")
 const router = express.Router()
 
 let movies = [
-        {
-            id: 1,
-            name: "Avenger",
-            rate: 8,
-            },
-        {
-            id: 2,
-            name: "Iron Man",
-            rate: 10,
-            },
-        {
-            id: 3,
-            name: "Thor",
-            rate: 10,
-            },
-        {
-            id: 4,
-            name: "Captain America",
-            rate: 4,
-        }
+        // {
+        //     id: 1,
+        //     name: "Avenger",
+        //     rate: 8,
+        //     },
+        // {
+        //     id: 2,
+        //     name: "Iron Man",
+        //     rate: 10,
+        //     },
+        // {
+        //     id: 3,
+        //     name: "Thor",
+        //     rate: 10,
+        //     },
+        // {
+        //     id: 4,
+        //     name: "Captain America",
+        //     rate: 4,
+        // }
     ]
 
-router.get("/movies", (req, res) => {
-    if (!req.query.rate) return res.json(movies);
-    const rate = Number(req.query.rate);
-    res.json(movies.filter(movie => movie.rate === rate));
+router.get("/movies", async (req, res) => {
+    const movies = await prisma.movies.findMany({
+        orderBy: {
+            rate: "desc"
+        },
+        where: {
+            rate: req.query.rate ? Number(req.query.rate) : undefined
+        }
+    })
+    res.json(movies)
 })
 
-router.get("/movie/:id", (req, res) => {
+router.get("/movie/:id", async (req, res) => {
     const id = Number(req.params.id);
-    const movie = movies.find((movie) => movie.id === id);
+    const movie = await prisma.movies.findFirst({
+        where: {
+            id
+        } // id : id
+    });
     // O "find" caso não encontre nada retorna "undefined", podíamos colocar no if (movie === undefined)..
     if (!movie) res.status(404).json({message: `Movie with id ${id} not found`})
     res.json(movie)
 })
 
-router.post("/movie", (req, res) => {
+router.post("/movie", async (req, res) => {
     const data = req.body
 
-    const movie = {
-        id: movies.length + 1,
-        name: data.name,
-        rate: data.rate
-    }
-    movies.push(movie);
+    const movie = await prisma.movies.create ({
+        data: {
+            name: data.name,
+            rate: data.rate
+        }
+    })
     res.status(201).json(movie);
 })
 
-router.put("/movie/:id", (req, res) => {
-    const id = req.params.id;
-    const data = req.body;
-    const movie = movies.find((movie) => movie.id === id);
-    if (!movie) return res.status(404).json({ message: "Movie not found"})
-    movie.name = data.name;
-    movie.rate = data.rate;
-    res.json(movie);
+router.put("/movie/:id", async (req, res) => {
+    const id = Number(req.params.id);
+    await prisma.movies.update({
+        data: {
+            name: req.body.name,
+            rate: req.body.rate,
+            description: req.body.description
+        },
+        where: {
+            id
+        }
+    })
+    res.json(movies)
 })
 
-router.delete("/movie/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const movie = movies.filter((movie => movie.id !== id));
-    res.status(204).send();
+router.delete("/movie/:id", async (req, res) => {
+    const id = Number(req.params.id)
+    await prisma.movies.delete({
+        where: {
+            id
+        }
+    })
+    res.status(204).send()
 })
 
 module.exports = {
